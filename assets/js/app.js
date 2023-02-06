a5244866e0d628fec2b77a828fc232a3d472aac3
ArannaBaura1971 <arannabaruase71@gmail.com>
2023-02-06 19:30:02 +0600

    calculator

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,18 @@ let buttons=document.querySelectorAll('button');
 
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
-        console.log(button);
-        strings=strings+button.innerHTML;
-        document.getElementById('in').innerHTML
+        if(e.target.innerHTML=="=") {
+            strings=eval(strings);
+            document.getElementById('in').value=strings;
+        }
+        else if(e.target.innerHTML=="AC")
+        {
+            strings="";
+            document.getElementById('in').value=strings;
+        }
+        else{
+            strings=strings+e.target.innerHTML;
+            document.getElementById('in').value=strings;
+        }
     })
 })
